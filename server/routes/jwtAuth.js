c923fb2adefe3e6235821bff7579ab00770d3d8c
RupiR <rupalirajput@gmail.com>
2020-08-05 17:03:24 -0500

    finished jwt auth and encryption via bcrypt

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const pool = require("../db")
+
+const pool = require("../db");
+const bcrypt = require("bcrypt");
 
 //registering 
 
@@ -16,13 +18,22 @@ const user = await pool.query("SELECT * FROM users WHERE user_email = $1",[
     email 
 ]);
 
-res.json(user.rows)
+if(user.rows.length !== 0){
+    return res.status(401).send("User already exists");
+}
 
 //3. Bycrypt the user password
+
+ const saltRound = 10; 
+ const salt = await bcrypt.genSalt(saltRounds);
+ const bcryptPassword = await bcrypt.hash(password, salt);
 
 
 //4. Enter the user inside our database
 
+const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3} RETURNING *", [name, email, bcryptPassword]);
+
+res.json(newUser.rows[0]);
 
 //5. Generating our jwt token
 
